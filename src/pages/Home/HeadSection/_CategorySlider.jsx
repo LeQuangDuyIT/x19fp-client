ae486574e947e0515ff233159a86f55e039eff7b
lqduycp <lqduycp@gmail.com>
2023-11-26 09:41:19 +0700

    Kaizen Swiper Slider

--- src/pages/Home/HeadSection/_CategorySlider.jsx
+++ src/pages/Home/HeadSection/_CategorySlider.jsx
@@ -1,3 +1,4 @@
+import { useRef } from 'react';
 import clsx from 'clsx';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Autoplay, Navigation } from 'swiper/modules';
@@ -7,37 +8,60 @@ import 'swiper/css';
 import 'swiper/css/pagination';
 import 'swiper/css/navigation';
 
+const SWIPER_BUTTON = {
+  NEXT: 'swiper-button-next',
+  PREV: 'swiper-button-prev'
+};
+
 const CategorySlider = ({ setBgImg }) => {
+  const swiperRef = useRef();
+
+  const onInit = Swiper => {
+    swiperRef.current = Swiper;
+  };
+
+  const SwiperConfig = {
+    slidesPerView: 5,
+    spaceBetween: 64,
+    navigation: {
+      nextEl: `.${SWIPER_BUTTON.NEXT}`,
+      prevEl: `.${SWIPER_BUTTON.PREV}`
+    },
+    autoplay: {
+      delay: 4000,
+      disableOnInteraction: false,
+      pauseOnMouseEnter: true
+    },
+    modules: [Autoplay, Navigation]
+  };
+
+  const onMouseEnter = thumbnail => {
+    setBgImg(thumbnail);
+    if (swiperRef.current) swiperRef.current.autoplay.stop();
+  };
+
+  const onMouseLeave = () => {
+    setBgImg(null);
+    if (swiperRef.current) swiperRef.current.autoplay.start();
+  };
+
   const swiperButtonClasses = 'bg-transparent text-white/40 hover:text-white';
 
   return (
     <div className='relative flex gap-8 my-8 z-10'>
-      <Swiper
-        slidesPerView={5}
-        spaceBetween={64}
-        navigation={{
-          nextEl: '.swiper-button-next',
-          prevEl: '.swiper-button-prev'
-        }}
-        autoplay={{
-          delay: 4000,
-          disableOnInteraction: false
-        }}
-        modules={[Autoplay, Navigation]}
-        className='w-[90%]'
-      >
+      <Swiper {...SwiperConfig} onInit={onInit} className='w-[90%]'>
         {categories.map(category => (
           <SwiperSlide key={category.title}>
             <CategoryThumbnail
               {...category}
-              onMouseEnter={() => setBgImg(category.thumbnail)}
-              onMouseLeave={() => setBgImg(null)}
+              onMouseEnter={() => onMouseEnter(category.thumbnail)}
+              onMouseLeave={onMouseLeave}
             />
           </SwiperSlide>
         ))}
       </Swiper>
-      <button className={clsx('swiper-button-prev', swiperButtonClasses)}></button>
-      <button className={clsx('swiper-button-next', swiperButtonClasses)}></button>
+      <button className={clsx(SWIPER_BUTTON.PREV, swiperButtonClasses)}></button>
+      <button className={clsx(SWIPER_BUTTON.NEXT, swiperButtonClasses)}></button>
     </div>
   );
 };
--- src/pages/Home/HeadSection/_rendering.js
+++ src/pages/Home/HeadSection/_rendering.js
@@ -11,40 +11,13 @@ import PhysicsThumbnailImg from '~/assets/category-thumbnail/physics.png';
 export const topSearchKeywords = ['Học sinh giỏi quốc gia', 'Tìm hiểu lịch sử', 'Javascript'];
 
 export const categories = [
-  {
-    title: 'Anh văn',
-    thumbnail: EnglishThumbnailImg
-  },
-  {
-    title: 'Địa lý',
-    thumbnail: GeographyThumbnailImg
-  },
-  {
-    title: 'Game',
-    thumbnail: GameThumbnailImg
-  },
-  {
-    title: 'Hóa học',
-    thumbnail: ChemistryThumbnailImg
-  },
-  {
-    title: 'Lịch sử',
-    thumbnail: HistoryThumbnailImg
-  },
-  {
-    title: 'Ngữ văn',
-    thumbnail: LiteratureThumbnailImg
-  },
-  {
-    title: 'Sinh học',
-    thumbnail: BiologyThumbnailImg
-  },
-  {
-    title: 'Tin học',
-    thumbnail: InformationTechnologyThumbnailImg
-  },
-  {
-    title: 'Vật lý',
-    thumbnail: PhysicsThumbnailImg
-  }
+  { title: 'Anh văn', thumbnail: EnglishThumbnailImg },
+  { title: 'Địa lý', thumbnail: GeographyThumbnailImg },
+  { title: 'Game', thumbnail: GameThumbnailImg },
+  { title: 'Hóa học', thumbnail: ChemistryThumbnailImg },
+  { title: 'Lịch sử', thumbnail: HistoryThumbnailImg },
+  { title: 'Ngữ văn', thumbnail: LiteratureThumbnailImg },
+  { title: 'Sinh học', thumbnail: BiologyThumbnailImg },
+  { title: 'Tin học', thumbnail: InformationTechnologyThumbnailImg },
+  { title: 'Vật lý', thumbnail: PhysicsThumbnailImg }
 ];
