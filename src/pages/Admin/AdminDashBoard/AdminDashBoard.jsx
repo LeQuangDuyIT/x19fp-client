e1b2a276b6d0255688b1b1a7af69f60546d1e938
duynhannguyen <130212806+duynhannguyen@users.noreply.github.com>
2023-12-15 03:39:48 +0700

    Change logic function find user

--- src/pages/Admin/AdminDashBoard/AdminDashBoard.jsx
+++ src/pages/Admin/AdminDashBoard/AdminDashBoard.jsx
@@ -1,14 +1,12 @@
-import { Row, Col, Divider, Select, Checkbox } from 'antd';
+import { Row, Col, Divider, Select, Checkbox, Popconfirm } from 'antd';
 import CountUp from 'react-countup';
 import { FaUsers } from 'react-icons/fa6';
 import { RiQuestionAnswerLine } from 'react-icons/ri';
 import { PiExam } from 'react-icons/pi';
 import Search from 'antd/es/input/Search';
-import { useEffect, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import accountAPI from '~/services/userAPI';
-import { useSelector } from 'react-redux';
 import LoadingState from '~/components/LoadingState/LoadingState';
-
 const dashBoard = [
   { title: 'NGƯỜI DÙNG', icon: <FaUsers />, quantity: '12360' },
   { title: 'CÂU HỎI/BÀI TẬP', icon: <RiQuestionAnswerLine />, quantity: '5294' },
@@ -16,8 +14,7 @@ const dashBoard = [
   { title: 'MÔN HỌC/BỘ SƯU TẬP', icon: <PiExam />, quantity: '78' }
 ];
 const AdminDashBoard = () => {
-  const [defaultUserList, setDefaultUserList] = useState();
-  const [userList, setUserList] = useState('');
+  const [userList, setUserList] = useState([]);
   const [searchKeyWord, setSearchKeyWord] = useState('');
   const [isChecked, setIschecked] = useState(false);
   useEffect(() => {
@@ -27,12 +24,12 @@ const AdminDashBoard = () => {
     try {
       const getAllUser = await accountAPI.getAllUser();
       const { data } = getAllUser.data;
-      setDefaultUserList(data);
       setUserList(data);
     } catch (error) {
       console.log(error);
     }
   };
+
   const dateToString = timeStamps => {
     const date = new Date(timeStamps);
     const options = { day: '2-digit', month: '2-digit', year: 'numeric' };
@@ -51,35 +48,28 @@ const AdminDashBoard = () => {
       }, delay);
     };
   };
-  const onHanleSearch = values => {
-    console.log(values);
-  };
-  // const onHanleChangeSearch = e => {
-  //   const lowerCaseKeyword = e.target.value.toLowerCase();
-  //   if (lowerCaseKeyword !== '') {
-  //     const filterResult = userList.filter(list => {
-  //       const result = list.lastName.toLowerCase().includes(lowerCaseKeyword);
-  //       return result;
-  //     });
-  //     return setUserList(filterResult);
-  //   } else {
-  //     return setUserList(defaultUserList);
-  //   }
-  // };
-  const onHanleChangeSearch = deBounce(e => {
-    const lowerCaseKeyword = e.target.value.toLowerCase();
-    if (lowerCaseKeyword !== '') {
-      const filterResult = userList.filter(list => {
-        const result = list.lastName.toLowerCase().includes(lowerCaseKeyword);
-        return result;
-      });
-      return setUserList(filterResult);
-    } else {
-      return setUserList(defaultUserList);
-    }
-  });
-  const onHanleChangeCheck = () => {
+
+  const filteredUserList = useMemo(
+    () =>
+      userList.filter(user => {
+        return user.lastName.toLowerCase().includes(searchKeyWord.toLowerCase());
+      }),
+    [searchKeyWord, userList]
+  );
+  // const filteredUserList = deBounce(
+  //   userList.filter(user => {
+  //     return user.lastName.toLowerCase().includes(searchKeyWord.toLowerCase());
+  //   }),
+  //   500
+  // );
+
+  const onHanleChangeCheckAll = () => {
     setIschecked(!isChecked);
+  };
+  const onHanleChangeCheckSingle = (value, id) => {
+    setIschecked(value);
+    console.log(value);
+    console.log(id);
   };
   return (
     <>
@@ -110,15 +100,36 @@ const AdminDashBoard = () => {
       <Row className='shadow border-gray-200 border-[1px] rounded-xl '>
         <Col className='leading-[50px] border-b border-gray-200' span={24}>
           <Row className='items-center text-gray-400 '>
-            <Col className='font-semibold pl-4 text-lg text-black' span={16}>
+            <Col className='font-semibold pl-4 text-lg text-black' span={12}>
               Danh sách tài khoản
             </Col>
 
-            <Col className='text-[13px]' span={8}>
+            <Col className='text-[13px] flex flex-grow  items-center gap-4 ' span={12}>
+              <div className=' w-[200px] h-[50px] '>
+                <div className='flex gap-4  items-center justify-center '>
+                  {isChecked && (
+                    <>
+                      <div>1 Selected</div>
+                      <Popconfirm
+                        title='Xóa đáp án'
+                        description='Bạn chắc chắn muốn xóa?'
+                        okText='Xóa'
+                        cancelText='Đóng'
+                        okButtonProps={{ danger: true }}
+                        // onConfirm={() => onHanleChangeCheckSingle()}
+                      >
+                        <div className='border-[1px] leading-tight cursor-pointer border-red-500 text-red-500 text-center rounded p-3 hover:bg-red-500 hover:text-white transition-all'>
+                          Delete
+                        </div>
+                      </Popconfirm>
+                    </>
+                  )}
+                </div>
+              </div>
               Tìm kiếm{' '}
               <Search
-                onChange={onHanleChangeSearch}
-                onSearch={onHanleSearch}
+                value={searchKeyWord}
+                onChange={e => setSearchKeyWord(e.target.value)}
                 size='small'
                 placeholder='Nhập tên người dùng'
                 className=' w-auto items-center align-middle '
@@ -129,7 +140,7 @@ const AdminDashBoard = () => {
         <Col span={24}>
           <Row className='text-gray-400 font-medium leading-10 border-b border-gray-200  '>
             <Col span={1} className='text-center'>
-              <Checkbox onChange={onHanleChangeCheck} />
+              <Checkbox onChange={onHanleChangeCheckAll} />
             </Col>
             <Col className='text-[13px]' span={5}>
               {' '}
@@ -151,12 +162,15 @@ const AdminDashBoard = () => {
           </Row>
         </Col>
         <Col className='leading-10' span={24}>
-          {userList ? (
-            userList.map(account => {
+          {filteredUserList ? (
+            filteredUserList.map(account => {
               return (
                 <Row key={account._id}>
                   <Col span={1} className='text-center'>
-                    <Checkbox id={account._id} />
+                    <Checkbox
+                      onChange={e => onHanleChangeCheckSingle(e.target.checked, account._id)}
+                      id={account._id}
+                    />
                   </Col>
                   <Col span={5}>
                     <div className='flex gap-3'>
